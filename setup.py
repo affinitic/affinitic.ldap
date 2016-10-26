a53aae824346584ceab87ad55f0c7c4c9c1c832b
AuroreMariscal <aurore@affinitic.be>
2016-10-26 13:08:03 +0200

    Add test-all parts
    This refs affinitic #8959

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ setup(name='affinitic.ldap',
       version=version,
       description="",
       long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
       keywords='',
       author='Affinitic',
       author_email='info@affinitic.be',
@@ -30,4 +30,6 @@ setup(name='affinitic.ldap',
       entry_points="""
       # -*- Entry points: -*-
       """,
+      extras_require=dict(
+          test=[]),
       )
